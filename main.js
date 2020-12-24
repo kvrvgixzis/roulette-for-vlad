1d75ed4a9cd6a8fe0998673781c85249168a63d9
Nikita Parfenov <n1koss.boy@gmail.com>
2020-12-24 19:41:27 +0500

    fix minor bugs

--- main.js
+++ main.js
@@ -1,66 +1,84 @@
 const btn = document.querySelector('.refresh-btn');
 const waitBtn = document.querySelector('.wait-btn');
 const roulette = document.querySelector('.roulette');
-
-const items = {
-  poroshochek: 0,
-  miska: 45,
-  eon: 90,
-  micro: 135,
-  tapki: 180,
-  vilka: 225,
-  tabachok: 270,
-  bitochek: 315,
-};
-
-const messages = {
-  poroshochek: document.querySelector('.poroshochek'),
-  miska: document.querySelector('.miska'),
-  eon: document.querySelector('.eon'),
-  micro: document.querySelector('.micro'),
-  tapki: document.querySelector('.tapki'),
-  vilka: document.querySelector('.vilka'),
-  tabachok: document.querySelector('.tabachok'),
-  bitochek: document.querySelector('.bitochek'),
-};
-
 const gap = 1080;
 let angle = 0;
-let btnActive = true;
+let isButtonEnabled = true;
 
-const randomItem = (obj) => {
-  const keys = Object.keys(obj);
+const items = {
+  poroshochek: {
+    deg: 0,
+    node: document.querySelector('.poroshochek'),
+  },
+  miska: {
+    deg: 45,
+    node: document.querySelector('.miska'),
+  },
+  eon: {
+    deg: 90,
+    node: document.querySelector('.eon'),
+  },
+  micro: {
+    deg: 135,
+    node: document.querySelector('.micro'),
+  },
+  tapki: {
+    deg: 180,
+    node: document.querySelector('.tapki'),
+  },
+  vilka: {
+    deg: 225,
+    node: document.querySelector('.vilka'),
+  },
+  tabachok: {
+    deg: 270,
+    node: document.querySelector('.tabachok'),
+  },
+  bitochek: {
+    deg: 315,
+    node: document.querySelector('.bitochek'),
+  },
+};
+
+const getRandomItemIndex = () => {
+  const keys = Object.keys(items);
   const key = keys[(keys.length * Math.random()) << 0];
   return key;
 };
 
-const offButton = () => {
-  btnActive = false;
+const disableButton = () => {
+  isButtonEnabled = false;
   btn.style.display = 'none';
   waitBtn.style.display = 'flex';
 };
 
-const onButton = () => {
-  btnActive = true;
+const enableButton = () => {
+  isButtonEnabled = true;
   btn.style.display = 'flex';
   waitBtn.style.display = 'none';
 };
 
-const rotateRoulette = () => {
-  if (!btnActive) return;
-  const selectedItem = randomItem(items);
-
-  Object.keys(messages).forEach((key) => {
-    messages[key].classList.remove('show');
+const hideNodes = () => {
+  Object.keys(items).forEach((key) => {
+    items[key].node.classList.remove('show');
   });
+};
 
-  offButton();
+const rotateRoulette = () => {
+  if (!isButtonEnabled) return;
+  hideNodes();
+  disableButton();
+
+  const randomItemIndex = getRandomItemIndex();
+  const randomItem = items[randomItemIndex];
+
   angle += gap;
-  roulette.style.transform = `rotate(-${items[selectedItem] + angle}deg)`;
+  roulette.style.transform = `rotate(-${randomItem.deg + angle}deg)`;
+
   setTimeout(() => {
-    onButton();
-    messages[selectedItem].classList.add('show');
-    delete items[selectedItem];
+    enableButton();
+    randomItem.node.classList.add('show');
+    delete items[randomItemIndex];
   }, 5000);
 };
 
